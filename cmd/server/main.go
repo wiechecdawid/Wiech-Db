8021376eaac283b3272863c447d4352d0a19aed7
wiechecdawid <wiechec.dawid@gmail.com>
2026-09-25 16:01:06 +0200

    Basic program loop

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,8 +2,36 @@ package main
 
 import (
 	"fmt"
+	"bufio"
+	"os"
+	"strings"
 )
 
 func main() {
-	fmt.Println("Hello, db!")
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Println("Hello, db! \".exit\" to quit")
+	for {
+		fmt.Print("db> ")
+		if !scanner.Scan() {
+			break
+		}
+
+		input := strings.TrimSpace(scanner.Text())
+
+		if input == "" {
+			continue
+		}
+
+		if input == ".exit" {
+			fmt.Println("Goodbye!")
+			os.Exit(0)
+		}
+
+		fmt.Printf("Command: '%s'\n", input)
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, "Unexpected error:", err)
+		os.Exit(1)
+	}
 }
